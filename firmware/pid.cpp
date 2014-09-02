b6438e1b4e8b0746b98fd882a90818dd52db0ec2
Dan Rice <dan@zoombody.com>
2014-09-01 22:11:13 -0400

    Clarify library origins and attribution

--- firmware/pid.cpp
+++ firmware/pid.cpp
@@ -2,6 +2,8 @@
  * Arduino PID Library - Version 1.0.1
  * by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
  *
+ * Adapted for Spark by Dan Rice <dan@zoombody.com>
+ *
  * This Library is licensed under a GPLv3 License
  **********************************************************************************************/
 
